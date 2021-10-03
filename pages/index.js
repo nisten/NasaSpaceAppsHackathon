722ff0528f73caab39959cc238b604a7f9276d9f
nisten <nisten@give2science.com>
2021-10-02 21:10:49 -0400

    webgl iframe works :)))

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { Container, Row, Col, Button, Card} from 'react-bootstrap'
 
 export default function Home() {
   return (
-    <Container>
-      Meow
-    </Container>
+      <Container>
+        
+        <Card>
+        <iframe src="../index.html"></iframe>
+        </Card>
+        <Button variant='secondary'>
+          Meow
+        </Button>
+      </Container>
+      
+      
+    
+    
   )
 }
