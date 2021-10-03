efdd8e6590064690800e27dcde7a5ae4249115d0
klaus <klaus@nc.local>
2021-10-03 13:15:22 -0400

    initbranch

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,8 @@ import { Container, Row, Col, Button, Card} from 'react-bootstrap'
 
 export default function Home() {
   return (
-      
-        
-        
+   
         <iframe src="../index.html" overflow="hidden" border="none" height="1000vh" width="100%" display="flex"></iframe>
-        
       
-      
-      
-      
-    
-    
   )
 }
