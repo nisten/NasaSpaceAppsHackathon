bcaf7a7b8b85d2a770ff1119968d1bf825df3676
nisten <nisten@give2science.com>
2022-05-21 09:17:41 -0400

    somehow it still works

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,14 @@ import { Container, Row, Col, Button, Card} from 'react-bootstrap'
 
 export default function Home() {
   return (
-      
-        
-        
-        <iframe src="../index.html" overflow="hidden" border="none" height="1000vh" width="100%" display="flex"></iframe>
-        
-      
-      
-      
-      
-    
-    
+    <div>
+      <Image
+      src="../transorbit.png"
+      alt="logo transparent background"
+      width={300}
+      height={65}
+    />
+    </div>,
+    <iframe src="../index.html" overflow="hidden" border="none" height="1000vh" width="100%" display="flex"></iframe>
   )
 }
